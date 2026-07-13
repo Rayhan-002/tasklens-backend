8b1b51d32fcda601e9c5a96fadb3722b67ef5f91
Md Rayhan Ali <sm.rayhanali.bd@gmail.com>
2026-07-13 21:58:07 +0600

    chore: production-ready settings (whitenoise, env vars, gunicorn, media serving)

--- core/urls.py
+++ core/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('auth_app.urls')),
     path('api/tasks/', include('tasks_app.urls')),
     path('api/annotations/', include('annotation_app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve uploaded media files in both dev and production
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
